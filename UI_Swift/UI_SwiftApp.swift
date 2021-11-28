8a73cc07398ec55c6b03403d33dbbebb47c65b8c
Shisetsu <shisetsu@icloud.com>
2021-11-28 15:51:52 +0300

    Lesson_1 Login View UI

---
+++ UI_Swift/UI_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_SwiftApp.swift
+//  UI_Swift
+//
+//  Created by Илья Дунаев on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct UI_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
